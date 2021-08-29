7ff7d8ee2e526b39b96d662e6e6e30b5e1282c32
Angel Banze <89379357+aebanze@users.noreply.github.com>
2021-08-29 11:47:23 +0200

    Update ArrayList.java

--- ArrayList/ArrayList.java
+++ ArrayList/ArrayList.java
@@ -1,14 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package listasligadas.Lists;
 
-/**
- *
- * @author Banze
- */
 public class ArrayList {
     private Object elementos [];
     private int totalElem;
